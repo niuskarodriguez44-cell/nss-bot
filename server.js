3d26b187a53aaeec213110ac6e783b85da9d30f5
niuskarodriguez44-cell <niuskarodriguez44@gmail.com>
2026-03-08 23:53:58 -0400

    server.js

--- server.js
+++ server.js
@@ -1,42 +1,9 @@
-const express = require("express");
 const admin = require("firebase-admin");
 
-const app = express();
-app.use(express.json());
-
-// leer credencial desde Railway
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
-
-app.get("/", (req, res) => {
-  res.send("NSS BOT funcionando");
-});
-
-// consultar inventario
-app.get("/producto/:nombre", async (req, res) => {
-  const nombre = req.params.nombre;
-
-  const snapshot = await db
-    .collection("productos")
-    .where("nombre", "==", nombre)
-    .get();
-
-  if (snapshot.empty) {
-    return res.json({ mensaje: "Producto no encontrado" });
-  }
-
-  const producto = snapshot.docs[0].data();
-
-  res.json(producto);
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Servidor NSS activo en puerto", PORT);
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
 });
